52e4255e3bcd7bdbe85db3db7e888a55b1e068a7
OleksandraKoshyk <115491825+OleksandraKoshyk@users.noreply.github.com>
2022-12-30 19:10:39 +0100

    change useState

--- src/Weather.js
+++ src/Weather.js
@@ -7,27 +7,24 @@ import "@fontsource/roboto"; // Defaults to weight 400.
 import axios from "axios";
 
 export default function Weather() {
-  function handleSearch(response) {
-    setTemperature(Math.round(response.data.temperature.current));
-    setWind(Math.round(response.data.wind.speed));
-    setHumidity(response.data.temperature.humidity);
-    setPressure(response.data.temperature.pressure);
-    setcityName(response.data.city);
-    setCountry(response.data.country);
-    setIcon(response.data.condition.icon_url);
-    setDescription(response.data.condition.description);
-  }
-  const [ready, setReady] = useState(false);
-  const [temperature, setTemperature] = useState("null");
-  const [description, setDescription] = useState("null");
-  const [cityName, setcityName] = useState("null");
-  const [country, setCountry] = useState("null");
-  const [wind, setWind] = useState("null");
-  const [humidity, setHumidity] = useState("null");
-  const [pressure, setPressure] = useState("null");
-  const [icon, setIcon] = useState("null");
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-  if (ready) {
+  function handleSearch(response) {
+    setWeatherData({
+      ready: true,
+      temperature: Math.round(response.data.temperature.current),
+      wind: Math.round(response.data.wind.speed),
+      humidity: response.data.temperature.humidity,
+      pressure: response.data.temperature.pressure,
+      cityName: response.data.city,
+      country: response.data.country,
+      icon: response.data.condition.icon_url,
+      description: response.data.condition.description,
+      dayOfWeek: "Saturday",
+    });
+  }
+
+  if (weatherData.ready) {
     return (
       <div className="container weather-app">
         <div className="row search-row">
@@ -57,31 +54,38 @@ export default function Weather() {
         <div className="row justify-content-between mt-md-5 weather-info-row">
           <div className="col-md-7 main-info">
             <h1>
-              <span className="{cityName}">{cityName},</span>{" "}
-              <span className="CountryName">{country}</span>
+              <span className="{cityName}">{weatherData.cityName},</span>{" "}
+              <span className="CountryName">{weatherData.country}</span>
             </h1>
-
-            <h3 className="DayOfWeek">DayOfWeek</h3>
-            <p>
-              <span>Hours</span>:<span>Minute</span>
+            <div className="date d-flex flex-row">
+              <h3 className="DayOfWeek text-capitalize pe-2">
+                {weatherData.dayOfWeek}
+              </h3>
+              <p className="align-self-start  time">
+                <span>{} Hours</span>:<span>Minute</span>
+              </p>
+            </div>
+            <p className="WeatherDescription text-capitalize">
+              {weatherData.description}
             </p>
-            <p className="WeatherDescription">{description}</p>
             <div className="temp-row d-flex flex-row align-items-start">
               <div className="Icon ">
                 <img
-                  src={icon}
+                  src={weatherData.icon}
                   className="img img-fluid weather-icon align-self-start"
                 />
               </div>
-              <div className="temp align-self-start">{temperature}</div>{" "}
+              <div className="temp align-self-start">
+                {weatherData.temperature}
+              </div>{" "}
               <div className="units">℃ | ℉</div>
             </div>
           </div>
           <div className="col-md-4 other-parameters d-flex justify-content-end align-items-end">
             <ul className="">
-              <li>Humidity: {humidity}%</li>
-              <li>Wind: {wind} km/h</li>
-              <li>Pressure: {pressure} hPa</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+              <li>Pressure: {weatherData.pressure} hPa</li>
             </ul>
           </div>
         </div>
@@ -91,7 +95,7 @@ export default function Weather() {
     let city = "Jelenia gora";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=4e3d43265f7f3448fot5bf7a6b40260b&units=metric`;
     axios.get(apiUrl).then(handleSearch);
-    setReady("true");
+
     return <h1>Loading..</h1>;
   }
 }
